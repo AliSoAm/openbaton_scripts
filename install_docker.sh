51455729f16a2a7e29de74ef8e7d63abb512207d
Unknown <ali.sorouramini@gmail.com>
2018-10-22 12:12:50 +0330

    redirect stdout to /dev/null
    This reverts commit 5e6fd6ef210428a4fe16c8b85d8b33bbec7e9831.

--- install_docker.sh
+++ install_docker.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
-sudo apt-get update
-
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update
-
-sudo apt-get install docker-ce
-
-sudo echo '{"registry-mirrors": ["http://repo.docker.ir:5000"]}' > /etc/docker/daemon.json
-
+sudo apt-get update>/dev/null
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common libltdl7>/dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add ->/dev/null
+sudo apt-key fingerprint 0EBFCD88>/dev/null
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable">/dev/null
+sudo apt-get update>/dev/null
+sudo apt-get install docker-ce>/dev/null
+sudo touch /etc/docker/daemon.json
+sudo echo '{"registry-mirrors": ["http://repo.docker.ir:5000"]}'> /etc/docker/daemon.json
 sudo service docker restart
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo groupadd docker
-
-sudo usermod -aG docker $USER
-
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose>/dev/null
+sudo chmod +x /usr/local/bin/docker-compose>/dev/null
+sudo groupadd docker>/dev/null
+sudo usermod -aG docker $USER>/dev/null
